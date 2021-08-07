91d3420d751a99fc6e4dce62652a96a7f520bb3d
Angel Bonilla <a01253832@itesm.mx>
2021-08-07 02:13:21 -0500

    Prueba de entorno

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import '../css/main.css';
+import '../img/recipes.jpg';
+import recipes from '../img/recipes.jpg';
+
+document.getElementById('ejemplo').src = recipes;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './index.html'
+    })
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+          test: /\.html$/i,
+          loader: 'html-loader',
+        },
+    ],
+  },
+}
